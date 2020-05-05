f3e5ded56bb6110eb8c69d318e5a628f659589fd
mattted <medward4@gmail.com>
2020-05-04 20:35:10 -0700

    Add edit form/action, update action, delete action, refactor controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,7 @@
 class CoursesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :destroy, :update]
+  before_action :set_course, only: [:show, :edit, :destroy, :update]
+  before_action :modify_course_permission, only: [:edit, :destroy, :update]
 
   def index
     @courses = Course.paginate(page: params[:page], per_page: 15)
@@ -11,24 +13,44 @@ class CoursesController < ApplicationController
 
   def create
     @course = current_user.created_courses.create(course_params)
-    if @course.valid?
-      redirect_to course_path(@course)
-    else
-      flash.now[:alert] = @course.errors.full_messages.join(', ')
-      render 'new'
-    end
+    error_check('new')
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    @course.update(course_params)
+    error_check('edit')
+  end
+
+  def destroy
+    @course.destroy
+    redirect_to courses_path, notice: "Course succesfully deleted."
+  end
+
+  private
+
+  def set_course
     @course = Course.find_by(id: params[:id])
     redirect_to courses_path, alert: "Sorry. That course does not exist." if @course.nil?
   end
 
-  def edit
-    
+  def modify_course_permission
+    redirect_to courses_path, alert: "Sorry. You can only modify courses you've created." if @course.creator_id != current_user.id
   end
 
-  private
+  def error_check(view)
+    if @course.valid?
+      redirect_to course_path(@course)
+    else
+      flash.now[:alert] = @course.errors.full_messages.join(', ')
+      render view 
+    end
+  end
   
   def course_params
     params.require(:course).permit(:name, :city, :state, :access, :style)
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,8 +1,8 @@
 class Course < ApplicationRecord
   has_many :rounds
   has_many :golfers, through: :rounds
-  has_many :tees, dependent: :destroy
-  has_many :holes, through: :tees
+  # has_many :tees, dependent: :destroy
+  # has_many :holes, through: :tees
   belongs_to :creator, class_name: "User", optional: true
 
   validates :name, presence: true 
