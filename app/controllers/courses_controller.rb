5461695f44a2113e1062d681f44c75de0338d39f
mattted <medward4@gmail.com>
2020-05-04 18:24:56 -0700

    Add new course form

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,7 +1,22 @@
 class CoursesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy, :update]
 
   def index
     @courses = Course.paginate(page: params[:page], per_page: 15)
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    binding.pry
+  end
+
+  private
+  
+  def course_params
+    params.require(:course).permit(:name, :city, :state, :access, :style)
   end
 
 end
