a31dfa3becc3cebd989f5fc1c487fab219d2ba30
mattted <medward4@gmail.com>
2020-05-10 18:57:16 -0700

    Reduce course seeds, increase round seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-user = User.create(email: "matt@test.com", name: "Matt", password: "testtest")
-user2 = User.create(email: "debbie@test.com", name: "Debbie", password: "testtest")
-user3 = User.create(email: Faker::Internet.email, name: Faker::Name.name, password: "testtest")
+user = User.create(email: "matt@test.com", name: "Matt", password: "testing")
+user2 = User.create(email: "debbie@test.com", name: "Debbie", password: "testing")
+user3 = User.create(email: Faker::Internet.email, name: "#{Faker::Name.first_name} #{Faker::Name::last_name}", password: "testing")
 
 # 20.times do
 #   User.create(email: Faker::Internet.email, name: Faker::Name.name, password: "testtest")
@@ -9,14 +9,14 @@ user3 = User.create(email: Faker::Internet.email, name: Faker::Name.name, passwo
 progress_count = 0
 
 User.all.each do |u|
-  50.times do
+  20.times do
     course = u.created_courses.create(name: Faker::Games::Zelda.location,
                                       city: Faker::Address.city,
                                       state: Faker::Address.state,
                                       style: %w[Links Desert Parkland].sample,
                                       access: %w[Public Semi-private Private Resort].sample)
     progress_count += 1
-    puts "Course Name: #{course.name} | #{progress_count.to_s}/150"
+    puts "Course Name: #{course.name} | #{progress_count.to_s}/60"
     %w[Black Blue White Gold Red].each do |color|
       tee = course.tees.create(color: color)
       puts "  #{tee.color} Tee"
@@ -32,11 +32,11 @@ User.all.each do |u|
 end
 
 User.all.each do |user|
-  20.times do
+  50.times do
     course = Course.all.sample
     round = user.rounds.create(date: Faker::Date.between(from: 1.year.ago, to: Date.today), course: course, tee: course.tees.sample.color) 
     18.times do |i|
-      round.scorecards.create(hole_number: i+1, fairway: Faker::Number.within(range: 0..1), gir: Faker::Number.within(range: 0..1), putts: Faker::Number.within(range: 1..3), strokes: Faker::Number.within(range: 3..6))
+      round.scorecards.create(hole_number: i+1, fairway: Faker::Number.within(range: 0..1), gir: Faker::Number.within(range: 0..1), putts: Faker::Number.within(range: 1..3), strokes: Faker::Number.within(range: 2..7))
     end
     puts course.name
     puts round.scorecards.pluck(:strokes).sum
